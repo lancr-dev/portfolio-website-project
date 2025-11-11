942932df8b290b23b1ed8d0ec38da28cbd4a1592
lans <ducantelanceandreio@gmail.com>
2025-11-11 09:07:13 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openHamburger(){
+    const hamburger = document.querySelector(".hamburger");
+    hamburger.style.display = "flex";
+}
+
+function closeHamburger(){
+    const hideHamburger = document.querySelector(".hamburger");
+    hideHamburger.style.display = "none";
+}
